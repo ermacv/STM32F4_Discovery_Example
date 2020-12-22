2baca08f5549c3a5675618398e0add7c4ba348dd
Vladislav Ermachenkov <vladislav.ermachenkov@electrolux.com>
2020-12-22 17:12:00 +0300

    Updated CMake common options

--- cmake/build_common/build_options.cmake
+++ cmake/build_common/build_options.cmake
@@ -1,34 +1,19 @@
 ###############################################################################
-# Common complier options
-set(compiler_common_flags "")
-set(compiler_common_flags "${compiler_common_flags} -fdata-sections")
-set(compiler_common_flags "${compiler_common_flags} -ffunction-sections" CACHE INTERNAL "")
+# Common compiler options
+add_compile_options(-fdata-sections)
+add_compile_options(-ffunction-sections)
+# Reproducibility option
+add_compile_options(-ffile-prefix-map=${CMAKE_SOURCE_DIR}=.)
 ###############################################################################
 # Common warnings
-set(compiler_common_warnings "-Wall")
-# set(compiler_common_warnings "${compiler_common_warnings} -pedantic")
-set(compiler_common_warnings "${compiler_common_warnings} -Wfatal-errors" CACHE INTERNAL "")
+add_compile_options(-Wall)
+add_compile_options(-Wfatal-errors)
 ###############################################################################
 # C standard
-set(PROJECT_C_STD "-std=c99" CACHE INTERNAL "")
+set(CMAKE_C_STANDARD 99)
+set(CMAKE_C_STANDARD_REQUIRED ON)
 ###############################################################################
 # CXX standard
-set(PROJECT_CXX_STD "-std=c++14" CACHE INTERNAL "")
-###############################################################################
-# Setup C options
-###############################################################################
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${PROJECT_C_STD}")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${compiler_common_flags}")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${compiler_common_warnings}")
-###############################################################################
-# Setup CXX options
-###############################################################################
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${PROJECT_CXX_STD}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${compiler_common_flags}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${compiler_common_warnings}")
-###############################################################################
-# Setup ASM options
-###############################################################################
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${compiler_common_flags}")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${compiler_common_warnings}")
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
 ###############################################################################
